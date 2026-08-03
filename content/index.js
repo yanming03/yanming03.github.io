ced5a7c1c8fa5e1f50ce0dc22b8caca3801da57e
yanming03 <yanming03@users.noreply.github.com>
2026-08-04 07:33:06 +0800

    Rewrite CapTwin as Per-Code-Act

--- content/index.js
+++ content/index.js
@@ -1,4 +1,4 @@
 window.CONTENT = [
   { id: '001', type: 'misc', title: 'Hello World', year: '2026', href: 'content/misc/hello-world/' },
-  { id: '002', type: 'blogs', title: 'CapTwin (0) Per-Act-Reflect', year: '2026', href: 'content/blogs/captwin-0-per-act-reflect/' }
+  { id: '002', type: 'blogs', title: 'CapTwin (0) Per-Code-Act', year: '2026', href: 'content/blogs/captwin-0-per-act-reflect/' }
 ];
